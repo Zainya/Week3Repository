d23fe52d8d1033e203e3e436bf178e293a37e79c
Zainya <77819437+Zainya@users.noreply.github.com>
2021-02-06 16:12:24 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+index.js
+
+var ages = [2, 3, 8, 9, 23, 28, 64, 93];
+
+
+
+var difference = (ages[lengthOfArr - lengthOfArr] - ages[lengthOfArr - 1]);
+
+
+
+console.log(`first and last element = difference`);
+
+
+names = ["Sam, Tommy, Tim, Sally,Buck, Bob"];
+sum = 0;
+for (i = 0; i < names.length; i++) {
+    sum += 'names'[i].length();
+}
+
+console.log('names')
+
+
+
+var array = [2, 3, 8, 9, 23, 28, 64, 93];
+var val = array[array.length - 1]; // 93
+console.log("[arr.length] to access the last element of an array");
+
+
+var array = [2, 3, 8, 9, 23, 28, 64, 93];
+var val = array[array.length - 1]; // 2
+console.log("[0] to access the first element of an array");
+
+
+
+
+console.log(duplicateWord("Hello", 5));
+
+
+
+console.log(getFullName("Zainya", "Mcgarrell"));
+
+
+
+
+
+
+
